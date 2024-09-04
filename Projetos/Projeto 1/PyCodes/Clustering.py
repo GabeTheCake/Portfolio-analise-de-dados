4cebe135a3ca7d6b22671cf6890d1ea96aa42969
Gabe <gabriel2006_11@hotmail.com>
2024-09-04 13:35:26 -0300

    Updated pycode

--- Projetos/Projeto 1/PyCodes/Clustering.py
+++ Projetos/Projeto 1/PyCodes/Clustering.py
@@ -1,36 +1,31 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
 import seaborn as sns
+from sklearn.cluster import KMeans
+from matplotlib.colors import ListedColormap
 
 #Loading the database
 db = pd.read_csv("C:/Users/conta/Portfolio/Projetos/Projeto 1/Dados/segmentation data.csv")
 
-#Dropping useless columns
+#Exploring the database
+print("Head\n", db.head(), "\n")
+print("Info\n", db.info(), "\n")
+print("Describe\n", db.describe(), "\n")
+print("Nunique\n", db.nunique(), "\n")
+
+#Dropping useless columns and saving columns name and order just in case
 db.drop(labels=["ID"],axis=1,inplace=True)
 CList = db.columns.tolist()
 
-#Exploring the database - shallow
-print(db.shape)
-print(db.head())
-print(db.describe())
-print(db.nunique())
-
-# HEATMAP
-corr = db.corr()
-plt.figure(figsize=(10, 8))
-sns.heatmap(corr, annot=True, cmap='coolwarm', center=0)
-plt.show()
-
-#Treating data - Seems to be no need for this DF to be cleanned. However I changed numbers from the legends archive to make it more easy to read and understand
+#Treating data
 db['Sex'] = db['Sex'].map({0:'Male', 1:'Female'})
 db['Sex'] = db['Sex'].astype('string')
 
 db['Marital status'] = db['Marital status'].map({0:'Single', 1:'Non-single'})
 db['Marital status'] = db['Marital status'].astype('string')
 
-db['Education'] = db['Education'].map({0:'other;unknown', 1:'High school', 2:'University', 3:'Graduate school'})
+db['Education'] = db['Education'].map({0:'Other/Unknown', 1:'High school', 2:'University', 3:'Graduate school'})
 db['Education'] = db['Education'].astype('string')
 
 db['Occupation'] = db['Occupation'].map({0:'Unemployed/Unskilled', 1:'Skilled employee/Official', 2:'Management/Self-employed/Highly qualified employee/Officer'})
@@ -39,16 +34,34 @@ db['Occupation'] = db['Occupation'].astype('string')
 db['Settlement size'] = db['Settlement size'].map({0:'Small city', 1:'Mid-sized city', 2:'Big city'})
 db['Settlement size'] = db['Settlement size'].astype('string')
 
-db['Age'] = db['Age'].astype(int)
 db['Income'] = db['Income'].astype(float)
 
-print("\n\n\n",db.dtypes,"\n\n\n")
+print("\n",db.dtypes,"\n")
 
-#EDA - deep
-cat_data = db.select_dtypes(include=['string'])
-for colname in cat_data.columns:
-    print (cat_data[colname].value_counts(), '\n')
+#EDA
+#cat_data = db.select_dtypes(include=['string'])
+#for colname in cat_data.columns:
+#    print (cat_data[colname].value_counts(), '\n')
+#
+#sns.countplot(x="Sex", hue="Matiral Status", data=db);
+#plt.show()
 
-sns.countplot(x="Marital status", hue="Sex", data=db);
-plt.savefig('Count_Marital_vs_Genre.png')
+#Clustering
+X = db[['Age','Income']]
+
+kmeans = KMeans(n_clusters=5, init='k-means++', max_iter=300, n_init=10, random_state=0)
+y_kmeans = kmeans.fit_predict(X)
+plt.scatter(X.values[y_kmeans == 0, 0], X.values[y_kmeans == 0, 1], s=100, c='red', label='Cluster 1')
+plt.scatter(X.values[y_kmeans == 1, 0], X.values[y_kmeans == 1, 1], s=100, c='blue', label='Cluster 2')
+plt.scatter(X.values[y_kmeans == 2, 0], X.values[y_kmeans == 2, 1], s=100, c='green', label='Cluster 3')
+plt.scatter(X.values[y_kmeans == 3, 0], X.values[y_kmeans == 3, 1], s=100, c='cyan', label='Cluster 4')
+plt.scatter(X.values[y_kmeans == 4, 0], X.values[y_kmeans == 4, 1], s=100, c='magenta', label='Cluster 5')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='yellow', label='Centroids')
+plt.title('Clusters de Clientes')
+plt.xlabel('Age')
+plt.ylabel('Income (anually)')
+plt.legend()
 plt.show()
+
+
+    
